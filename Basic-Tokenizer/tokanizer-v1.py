978aef95d5ac3de3eba11b94b9f5626325785ef6
Fazmin Nizam <fazmin.nizam@gmail.com>
2023-06-15 22:17:08 -0400

    first commit

---
+++ Basic-Tokenizer/tokanizer-v1.py
@@ -0,0 +1,35 @@
+import re
+
+class SimpleTokenizer:
+    def __init__(self, split_char=' ', remove_punctuation=True, to_lower=True):
+
+        self.split_char = split_char
+        self.remove_punctuation = remove_punctuation
+        self.to_lower = to_lower
+
+    def tokenize(self, text):
+
+        if self.to_lower:
+            text = text.lower()
+            
+        if self.remove_punctuation:
+            text = re.sub(r'[^\w\s]', '', text)
+
+        tokens = text.split(self.split_char)
+
+        return tokens
+
+    def tokenize_file(self, file_path):
+
+        tokens = []
+
+        with open(file_path, 'r') as file:
+            for line in file:
+                tokens.append(self.tokenize(line))
+
+        return tokens
+
+tokenizer = SimpleTokenizer(split_char=' ', remove_punctuation=True, to_lower=True)
+tokens = tokenizer.tokenize_file('path_to_your_file.txt')
+print(tokens)
+
---
+++ README.md
@@ -0,0 +1,7 @@
+### This just simple tokenizer takes a string of text and breaks it up into individual tokens. 
+ - The settings can be adjusted to split on different characters
+ - Options for remove or not remove punctuation, 
+ - Option to convert all text to lowercase or leave it as is.
+
+Please note that this is a very basic tokenizer I wrote to test out the process.
+For more complex use cases such as handling different languages, contractions, or more advanced tokenization strategies, consider using established libraries like NLTK, SpaCy, or Hugging Face's Tokenizers.
